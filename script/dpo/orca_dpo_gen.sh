a2ea8867820dfa74b9c2da91c15217c6f214c656
HeegyuKim <heekue83@gmail.com>
2024-06-05 05:07:09 +0000

    gemma template 추가

--- script/dpo/orca_dpo_gen.sh
+++ script/dpo/orca_dpo_gen.sh
@@ -1,11 +1,11 @@
 
 mkdir -p data/
-# python -m src.generate_response \
-#     --model yanolja/EEVE-Korean-Instruct-2.8B-v1.0 --chat_template eeve
+python -m src.generate_response \
+    --model yanolja/EEVE-Korean-Instruct-10.8B-v1.0 --chat_template eeve
 # python -m src.generate_response \
 #     --model google/gemma-1.1-2b-it --eos_token "<end_of_turn>"
 
-python -m src.generate_response \
-    --model heegyu/beomi__gemma-ko-2b-gemma-ko-2b-0416@steps-499999 --eos_token "<end_of_turn>" --chat_template gemma
+# python -m src.generate_response \
+#     --model heegyu/beomi__gemma-ko-2b-gemma-ko-2b-0416@steps-499999 --eos_token "<end_of_turn>" --chat_template gemma
 
     
